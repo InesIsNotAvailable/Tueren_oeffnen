91dfb0fdc779ef7a45dc2676462e9300dc127b53
InesIsNotAvailable <ines.salzburg@gmail.com>
2023-03-08 17:36:47 +0100

    buttons uwu

---
+++ Assets/MyGame/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI push;
+    public TextMeshProUGUI pull;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
